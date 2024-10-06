48c874a6b604b6575b2d9b6d18bbb2ebd69f928f
Nilesh kumar <71492461+neilkumar93600@users.noreply.github.com>
2024-10-06 23:29:29 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'deep-blue': '#0B132B',
+        'dark-blue': '#1C2541',
+        'bright-pink': '#F72585',
+      },
+      textColor: {
+        'gradient': 'linear-gradient(45deg, #ff0080, #ff8c00)', // For text gradient
+      },
+    },
   },
   plugins: [],
 };
